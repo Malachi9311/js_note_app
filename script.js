7c990a83e9d90d67da7db1c382f011fd9b055cf7
Malachi9311 <ntuthuko93dlamini@gmail.com>
2024-03-07 16:18:43 +0200

    dynamically adding input-boxes

---
+++ script.js
@@ -0,0 +1,16 @@
+const notesContainer = document.querySelector('.note-container');
+const createBtn = document.querySelector('.btn');
+let notes = document.querySelectorAll('.input-box');
+
+const createNote = () => {
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.classList = "input-box";
+    inputBox.setAttribute("contenteditable","true")
+    img.src = "images/delete.png";
+    inputBox.append(img);
+    notesContainer.appendChild(inputBox);
+    // notesContainer.appendChild(inputBox).appendChild(img);
+}
+
+createBtn.addEventListener('click', createNote);

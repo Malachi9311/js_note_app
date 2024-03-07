e15792f4473ff967dbea8144b669f8b4ac99069a
Malachi9311 <ntuthuko93dlamini@gmail.com>
2024-03-07 17:21:13 +0200

    local storage save debug

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const notesContainer = document.querySelector('.note-container');
 const createBtn = document.querySelector('.btn');
 let notes = document.querySelectorAll('.input-box');
 
+const showNotes = () => {
+    notesContainer.innerHTML = localStorage.getItem("notes");
+};
+showNotes();
+
+const updateStorage = () => {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+};
+
 const createNote = () => {
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
@@ -16,9 +25,24 @@ const createNote = () => {
 createBtn.addEventListener('click', createNote);
 
 const removeNote = (e) => {
-    if(e.target.tagName === "IMG") {
+    if(e.target.nodeName === "IMG") {
         e.target.parentElement.remove();
+        updateStorage();
+    }else if (e.target.nodeName === "P") {
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(note => {
+            note.onkeyup = function(){
+                updateStorage();
+            }
+        })
     }
 };
 
 notesContainer.addEventListener('click', removeNote);
+
+document.addEventListener("keydown", event => {
+    if (event.key === "Enter") {
+        document.execCommand("insertLineBreak")
+        event.preventDefault()
+    }
+})
